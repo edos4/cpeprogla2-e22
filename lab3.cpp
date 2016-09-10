d6015ebbdb209bf62b484a5810907f27e93cc013
Ed Salinas <edsalinas7b@yahoo.com>
2016-09-10 11:27:17 +0800

    Adds custom strcpy

--- lab3.cpp
+++ lab3.cpp
@@ -17,9 +17,15 @@ char* myStrCmp(char* str1, char* str2){
   }
 }
 
+void myStrCpy(char* str1, char* str2){
+  strcpy(str1, str2);
+}
+
 int main(){
   char str1[100]="Hello";
   char str2[100]="hello";
   
   myStrCmp(str1, str2);
+  myStrCpy(str1, str2);
+  cout << str1;
 }
